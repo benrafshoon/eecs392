f5dc147efd2e8623e7e3bd75b7600c9f9bacc746
Ben Rafshoon <benraf@u.northwestern.edu>
2014-05-14 19:17:54 -0500

    Matlab code to convert images to SRAM hex format

---
+++ image2hex/hex2display.m
@@ -0,0 +1,18 @@
+function hex2display()
+
+[input_filename, pathname, filter_index] = uigetfile('*.hex');
+if(filter_index == 0)
+    return
+end
+
+file = fopen(strcat(pathname, input_filename), 'r');
+
+array = fscanf(file, '%x\n', [1 Inf]);
+
+fclose(file);
+
+matrix = transpose(reshape(array, [640, 480]));
+
+imshow(matrix, 'DisplayRange', [0, 255]);
+
+imwrite(uint8(matrix), 'output.hex.png');
---
+++ image2hex/image2hex.m
@@ -0,0 +1,26 @@
+function image2hex()
+
+[input_filename, pathname, filter_index] = uigetfile('*');
+if(filter_index == 0)
+    return
+end
+
+splitfile = strsplit(input_filename, '.');
+if(length(splitfile) > 1)
+    splitfile = splitfile(1:end-1);
+end
+
+splitfile{length(splitfile) + 1} = 'hex';
+
+output_filename = strjoin(splitfile, '.');
+
+image = imresize(rgb2gray(imread(strcat(pathname, input_filename))), [480, 640]);
+
+file = fopen(strcat(pathname, output_filename), 'w+');
+for y = 1:size(image, 1)
+    for x = 1:size(image, 2)
+        fprintf(file, '%x\n', image(y, x));
+    end
+end
+
+fclose(file);
